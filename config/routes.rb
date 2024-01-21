68d901dc0f08c21f4abf22f65c4db49e69830c72
DaniaErnest <daniaernest@aol.com>
2024-01-21 10:06:32 +0100

    New-User SignUp Form

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'pages#home'
-
   get "about", to: 'pages#about'
   resources :articles
+  get "signup", to: 'users#new'
+  resources :users, except: [:new]
 end
